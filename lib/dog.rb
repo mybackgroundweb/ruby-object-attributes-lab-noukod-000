8c1b79d74d0b73de7a070e94454b73f1dfff9ce7
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-08 19:45:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,10 @@
-class Dog = (name)
-  @name = name
+1.	class Dog
+2.	 
+3.	  def name=(dogs_name)
+4.	    @this_dogs_name = dogs_name
+5.	  end
+6.	 
+7.	  def name
+8.	    @this_dogs_name
+9.	  end
+10.	end
