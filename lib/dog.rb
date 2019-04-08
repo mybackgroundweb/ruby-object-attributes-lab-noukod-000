5607eb07af6943cfbd1f41a3e5c0b126ad0f3585
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-08 19:49:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,13 @@ class Dog
   def name=(dogs_name)
     @name = dogs_name
   end
-  
-  def breed = (dogs_breed)
-    @breed = dogs_breed
-  end
  
   def name
     @name
+  end
+  
+  def breed = (dogs_breed)
+    @breed = dogs_breed
   end
   
   def breed
